f52d3b5948d5bb258e679ea412d6ceef584b8182
Ahmed-Abdella <abdellaa921@gmail.com>
2022-11-11 05:21:15 +0200

    Using compostion to create reusable code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { MatchReader } from "./MatchReader";
-
 import { MatchResult } from "./MatchResult";
 
-const reader = new MatchReader("football.csv");
-reader.read();
+import { CsvFileReader } from "./CsvFileReader";
 
-console.log(reader.data[0][0]);
+import { MatchReader } from "./MatchReader";
+
+const csvFileReader = new CsvFileReader("football.csv");
+csvFileReader.read();
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+console.log(matchReader.matches[0][0]);
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
